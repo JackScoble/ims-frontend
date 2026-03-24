ccb5a0bbae04f919f57301009803bf85cf8d0a96
JackScoble <jack.scoble@ada.ac.uk>
2026-03-24 12:40:15 +0000

    Create Analytics placeholder for the router

---
+++ src/components/pages/Analytics.jsx
@@ -0,0 +1,9 @@
+const Analytics = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Data Visualization</h1>
+      <p>Charts to go here</p>
+    </div>
+  );
+};
+export default Analytics;
